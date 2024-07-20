76aedee912b0a51efef068fbc2d3efb1ceda4037
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-07-20 19:47:44 +0530

    Add files via upload

---
+++ 24 07 20 LinkedList Reverse LinkedList O N O 1/src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    Node head;
+    public void add(int data){
+        if(head == null){
+            head = new Node(data);
+            return;
+        }
+        Node current = head;
+        while(current.next != null){
+            current = current.next;
+        }
+        current.next = new Node(data);
+    }
+    public void reverse(){
+        Node previous = null;
+        Node current = head;
+        Node next = null;
+        while(current != null){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+    }
+
+    public void display(){
+        Node current = head;
+        while(current != null){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the elements: ");
+        for(int i = 0; i< n; i++){
+            list.add(sc.nextInt());
+        }
+
+        System.out.println("Original LinkedList: ");
+        list.display();
+        list.reverse();
+        System.out.println("Reversed Linked List: ");
+        list.display();
+    }
+
+}
